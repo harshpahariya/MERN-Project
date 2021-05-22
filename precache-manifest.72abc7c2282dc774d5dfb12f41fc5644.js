4de701733092321fa93eb6db47ac98ee62fb74a2
Harsh Pahariya <harsh.pahariya78@gmail.com>
2021-05-22 13:11:55 +0530

    Updates

---
+++ precache-manifest.72abc7c2282dc774d5dfb12f41fc5644.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28e2b72d4a273c38c9581e7c976f7e5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c7a059b9731af59d2ef7",
+    "url": "/portfolio/static/css/2.36b72570.chunk.css"
+  },
+  {
+    "revision": "f81c99b59f89f428c368",
+    "url": "/portfolio/static/css/main.8b3280d3.chunk.css"
+  },
+  {
+    "revision": "c7a059b9731af59d2ef7",
+    "url": "/portfolio/static/js/2.5c51a3ea.chunk.js"
+  },
+  {
+    "revision": "67103adef8371e97262c4460a4f586be",
+    "url": "/portfolio/static/js/2.5c51a3ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f81c99b59f89f428c368",
+    "url": "/portfolio/static/js/main.d8ca0918.chunk.js"
+  },
+  {
+    "revision": "0441743606cc8b21f0f3",
+    "url": "/portfolio/static/js/runtime-main.381687a1.js"
+  }
+]);
